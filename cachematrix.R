fa6644370c61bddf78e0bd25fa68cdb6a5ea0cbc
matiasvalenzuelamunoz <valenzuelamatias26@gmail.com>
2020-07-18 12:43:02 -0400

    this is the complete assignament work for the lexical scopping , i have to recognice that despite having spent almost 3 days reading articles and seeing classes over and over again, it is difficult for me to understand what programming is like. The example helped me, but to be honest, I don't think I could have done it without it.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        minv <- NULL
+        set <- function(y) {
+                x <<- y
+                minv <<- NULL
+        #set values of the matrix
+        }
+        get <- function() x
+        #get the values of the matrix
+        setinverse <- function(inverse) minv <<- inverse
+        #set the values of the mean
+        getinverse <- function() minv
+        #get the values of the mean
+        list ( set = set, get = get,
+               setinverse = setinverse, getinverse = getinverse )
+        
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) { 
+        minv <- x$getinverse()
+        if(!is.null(minv)){
+                message(" getting cached data")
+                return(minv)
+        }
+        data <- x$get()
+        minv <- solve(data, ...)
+        x$setinverse(minv)
+        minv
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+cacheSolve(test)
